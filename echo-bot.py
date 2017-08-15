4c71cf97d7cdac81f40eabae9b952022faae7f77
Arthur <archcores@gmail.com>
2017-08-15 18:50:56 +0500

    echo bot finished

--- echo-bot.py
+++ echo-bot.py
@@ -6,6 +6,11 @@ import telebot
 
 bot = telebot.TeleBot(config_echo.token)
 
+# обработчик, который реагирует на все входящие сообщения
 @bot.message_handler(content_types=["text"])
 def repeat_messages(message):
 	bot.send_message(message.chat.id, message.text)
+
+# запускаем бесконечный цикл получения новых записей со стороны Telegram
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
